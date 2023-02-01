2a3553705a1f56e7bf634251d84978446486c1ca
longnt <longnt@bizflycloud.vn>
2023-02-01 14:34:25 +0700

    authen commit

--- function/tele.py
+++ function/tele.py
@@ -2,9 +2,6 @@ import os
 import telebot
 from api import get_response
 
-API_KEY = '5176546940:AAGv5n9ujFCE41AiHhuQ4cWHbEHB2x3S4H0'
-chat_id = -493944553
-
 bot = telebot.TeleBot(API_KEY)
 
 @bot.message_handler(commands=['start'])
@@ -20,11 +17,3 @@ def chat_gpt(message):
     bot.reply_to(message, str(get_response(message.text)))
 
 bot.infinity_polling()
-
-# if __name__ == '__main__':
-    # logfile = open(file_path,"r")
-    # loglines = follow(logfile)
-    # for line in loglines:
-        # print(line)
-        # print(type(line))
-        # bot.send_message(chat_id, "New log: {log}".format(log=line))
